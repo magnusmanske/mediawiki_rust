8f8e89dbee58545fb5b3d6a8e572b28cd7e2f93b
Magnus Manske <magnusmanske@googlemail.com>
2026-03-11 13:32:06 +0000

    action_logout: logout action module

---
+++ src/action_api/action_logout.rs
@@ -0,0 +1,76 @@
+use super::{ActionApiData, ActionApiRunnable, NoTitlesOrGenerator, Runnable};
+use std::{collections::HashMap, marker::PhantomData};
+
+pub(crate) type NoToken = super::NoTitlesOrGenerator;
+
+#[derive(Debug, Clone, Default)]
+pub struct ActionApiLogoutData {
+    token: Option<String>,
+}
+
+impl ActionApiData for ActionApiLogoutData {}
+
+impl ActionApiLogoutData {
+    pub(crate) fn params(&self) -> HashMap<String, String> {
+        let mut params = HashMap::new();
+        params.insert("action".to_string(), "logout".to_string());
+        Self::add_str(&self.token, "token", &mut params);
+        params
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct ActionApiLogoutBuilder<T> {
+    _phantom: PhantomData<T>,
+    pub(crate) data: ActionApiLogoutData,
+}
+
+impl ActionApiLogoutBuilder<NoToken> {
+    pub fn new() -> Self {
+        Self {
+            _phantom: PhantomData,
+            data: ActionApiLogoutData::default(),
+        }
+    }
+
+    pub fn token<S: AsRef<str>>(mut self, token: S) -> ActionApiLogoutBuilder<Runnable> {
+        self.data.token = Some(token.as_ref().to_string());
+        ActionApiLogoutBuilder {
+            _phantom: PhantomData,
+            data: self.data,
+        }
+    }
+}
+
+impl ActionApiRunnable for ActionApiLogoutBuilder<Runnable> {
+    fn params(&self) -> HashMap<String, String> {
+        self.data.params()
+    }
+
+    fn http_method(&self) -> &'static str {
+        "POST"
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn token_set() {
+        let params = ActionApiLogoutBuilder::new().token("csrf123").data.params();
+        assert_eq!(params["token"], "csrf123");
+    }
+
+    #[test]
+    fn action_is_logout() {
+        let params = ActionApiLogoutBuilder::new().token("tok").data.params();
+        assert_eq!(params["action"], "logout");
+    }
+
+    #[test]
+    fn http_method_is_post() {
+        let builder = ActionApiLogoutBuilder::new().token("tok");
+        assert_eq!(builder.http_method(), "POST");
+    }
+}
