61530ff547e4222d52c02663189e35d2559fbbbe
Magnus Manske <magnusmanske@googlemail.com>
2026-03-11 12:39:21 +0000

    list_imageusage list module

---
+++ src/action_api/list_imageusage.rs
@@ -0,0 +1,193 @@
+use super::{ActionApiData, ActionApiGenerator, ActionApiRunnable, NoTitlesOrGenerator, Runnable};
+use crate::api::NamespaceID;
+use std::{collections::HashMap, marker::PhantomData};
+
+#[derive(Debug, Clone)]
+pub struct ActionApiListImageusageData {
+    iutitle: Option<String>,
+    iupageid: Option<u64>,
+    iucontinue: Option<String>,
+    iunamespace: Option<Vec<NamespaceID>>,
+    iudir: Option<String>,
+    iufilterredir: Option<String>,
+    iulimit: usize,
+    iuredirect: bool,
+}
+
+impl ActionApiData for ActionApiListImageusageData {}
+
+impl Default for ActionApiListImageusageData {
+    fn default() -> Self {
+        Self {
+            iutitle: None,
+            iupageid: None,
+            iucontinue: None,
+            iunamespace: None,
+            iudir: None,
+            iufilterredir: None,
+            iulimit: 10,
+            iuredirect: false,
+        }
+    }
+}
+
+impl ActionApiListImageusageData {
+    pub(crate) fn params(&self) -> HashMap<String, String> {
+        let mut params = HashMap::new();
+        Self::add_str(&self.iutitle, "iutitle", &mut params);
+        if let Some(iupageid) = self.iupageid {
+            params.insert("iupageid".to_string(), iupageid.to_string());
+        }
+        Self::add_str(&self.iucontinue, "iucontinue", &mut params);
+        if let Some(ns) = &self.iunamespace {
+            let s: Vec<String> = ns.iter().map(|n| n.to_string()).collect();
+            params.insert("iunamespace".to_string(), s.join("|"));
+        }
+        Self::add_str(&self.iudir, "iudir", &mut params);
+        Self::add_str(&self.iufilterredir, "iufilterredir", &mut params);
+        params.insert("iulimit".to_string(), self.iulimit.to_string());
+        Self::add_boolean(self.iuredirect, "iuredirect", &mut params);
+        params
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct ActionApiListImageusageBuilder<T> {
+    _phantom: PhantomData<T>,
+    pub(crate) data: ActionApiListImageusageData,
+}
+
+impl<T> ActionApiListImageusageBuilder<T> {
+    pub fn iunamespace(mut self, iunamespace: &[NamespaceID]) -> Self {
+        self.data.iunamespace = Some(iunamespace.to_vec());
+        self
+    }
+
+    pub fn iudir<S: AsRef<str>>(mut self, iudir: S) -> Self {
+        self.data.iudir = Some(iudir.as_ref().to_string());
+        self
+    }
+
+    pub fn iufilterredir<S: AsRef<str>>(mut self, iufilterredir: S) -> Self {
+        self.data.iufilterredir = Some(iufilterredir.as_ref().to_string());
+        self
+    }
+
+    pub fn iulimit(mut self, iulimit: usize) -> Self {
+        self.data.iulimit = iulimit;
+        self
+    }
+
+    pub fn iuredirect(mut self, iuredirect: bool) -> Self {
+        self.data.iuredirect = iuredirect;
+        self
+    }
+}
+
+impl ActionApiListImageusageBuilder<NoTitlesOrGenerator> {
+    pub fn new() -> Self {
+        Self {
+            _phantom: PhantomData,
+            data: ActionApiListImageusageData::default(),
+        }
+    }
+
+    pub fn iutitle<S: AsRef<str>>(mut self, iutitle: S) -> ActionApiListImageusageBuilder<Runnable> {
+        self.data.iutitle = Some(iutitle.as_ref().to_string());
+        ActionApiListImageusageBuilder {
+            _phantom: PhantomData,
+            data: self.data,
+        }
+    }
+
+    pub fn iupageid(mut self, iupageid: u64) -> ActionApiListImageusageBuilder<Runnable> {
+        self.data.iupageid = Some(iupageid);
+        ActionApiListImageusageBuilder {
+            _phantom: PhantomData,
+            data: self.data,
+        }
+    }
+}
+
+impl ActionApiGenerator for ActionApiListImageusageBuilder<NoTitlesOrGenerator> {
+    fn generator_params(&self) -> HashMap<String, String> {
+        let mut params = Self::prefix_params('g', self.data.params());
+        params.insert("generator".to_string(), "imageusage".to_string());
+        params
+    }
+}
+
+impl ActionApiRunnable for ActionApiListImageusageBuilder<Runnable> {
+    fn params(&self) -> HashMap<String, String> {
+        let mut ret = self.data.params();
+        ret.insert("action".to_string(), "query".to_string());
+        ret.insert("list".to_string(), "imageusage".to_string());
+        ret
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{Api, action_api::ActionApiList};
+
+    fn new_builder() -> ActionApiListImageusageBuilder<NoTitlesOrGenerator> {
+        ActionApiListImageusageBuilder::new()
+    }
+
+    #[test]
+    fn default_iulimit_is_10() {
+        let params = new_builder().iutitle("File:Foo.jpg").data.params();
+        assert_eq!(params["iulimit"], "10");
+    }
+
+    #[test]
+    fn iutitle_set() {
+        let params = new_builder().iutitle("File:Einstein1921.jpg").data.params();
+        assert_eq!(params["iutitle"], "File:Einstein1921.jpg");
+    }
+
+    #[test]
+    fn iupageid_set() {
+        let params = new_builder().iupageid(12345).data.params();
+        assert_eq!(params["iupageid"], "12345");
+    }
+
+    #[test]
+    fn iunamespace_set() {
+        let params = new_builder().iunamespace(&[0]).iutitle("File:Foo.jpg").data.params();
+        assert_eq!(params["iunamespace"], "0");
+    }
+
+    #[test]
+    fn iulimit_set() {
+        let params = new_builder().iulimit(50).iutitle("File:Foo.jpg").data.params();
+        assert_eq!(params["iulimit"], "50");
+    }
+
+    #[test]
+    fn iuredirect_true() {
+        let params = new_builder().iuredirect(true).iutitle("File:Foo.jpg").data.params();
+        assert!(params.contains_key("iuredirect"));
+    }
+
+    #[test]
+    fn runnable_params_contain_action_list() {
+        let builder = new_builder().iutitle("File:Foo.jpg");
+        let params = ActionApiRunnable::params(&builder);
+        assert_eq!(params["action"], "query");
+        assert_eq!(params["list"], "imageusage");
+    }
+
+    #[tokio::test]
+    async fn test_imageusage() {
+        let api = Api::new("https://en.wikipedia.org/w/api.php").await.unwrap();
+        let result = ActionApiList::imageusage()
+            .iutitle("File:Einstein1921 by F Schmutzer 2.jpg")
+            .iulimit(5)
+            .run(&api)
+            .await
+            .unwrap();
+        assert!(result["query"]["imageusage"].is_array());
+    }
+}
